0de557c4ca3589af22a88c1437ee1e7a7ca5abe8
chana <chana@10.214.34.37>
2024-10-22 14:59:09 -0700

    New message

--- src/PlayerButton.java
+++ src/PlayerButton.java
@@ -1,6 +1,24 @@
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 
 public class PlayerButton extends JButton
 {
-
+	private char letter;
+	
+	public PlayerButton(char letter)
+	{
+		super(String.valueOf(letter));
+		this.letter = letter;
+	}
+	
+	public char getLetter()
+	{
+		return letter;
+	}
+	
+	public void addWordListener(ActionListener listener)
+	{
+		this.addActionListener(listener);
+	}
 }
--- src/WordPuzzleGame.java
+++ src/WordPuzzleGame.java
@@ -1,5 +1,35 @@
+import java.awt.BorderLayout;
 
-public class WordPuzzleGame
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class WordPuzzleGame extends JFrame
 {
+	private JLabel displayLabel;
+	private char[] guessedWord;
 
+	public WordPuzzleGame()
+	{
+		setTitle("Word Puzzle Game");
+		setSize(600,600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+		
+		selectNewWord();
+		
+		displayLabel = new JLabel(getDisplayWord(), SwingConstants.CENTER);
+		displayLabel.setFont(new Font("Arial", Font));
+	}
+
+	private String getDisplayWord()
+	{
+		return String.valueOf(guessedWord);
+	}
 }
