998d263fba2d0d711a18dc1d68b9ba8e411bb569
chana <chana@JavisLenovo>
2024-12-10 15:55:19 -0800

    update

--- src/PlayerButton.java
+++ src/PlayerButton.java
@@ -1,25 +1,8 @@
-/**
- * PlayerButton.java
- * 
- * Purpose:
- * - Represents a clickable button for a specific letter in the Word Puzzle Game.
- * - Extends JButton to associate a letter with the button.
- * 
- * Features:
- * - Stores the letter it represents.
- * - Provides a method to retrieve the letter.
- * - Allows adding an ActionListener for user interaction.
- * 
- * Author: Christopher OShea & Javier Ayala
- * Date: 10 December 2024
- */
-import javax.swing.*;
-import java.awt.event.ActionListener;
+import java.awt.event.ActionListener; 
 
-public class PlayerButton extends JButton {
+import javax.swing.JButton;
 
-<<<<<<< HEAD
-public class PlayerButton extends JButton //A PlayerButton represents a clickable letter button in the game. 
+public class PlayerButton extends JButton
 {
 	private char letter;//a PlayerButton has a letter
 	
@@ -50,35 +33,4 @@ public class PlayerButton extends JButton //A PlayerButton represents a clickabl
 	{
 		this.addActionListener(listener);
 	}
-=======
-    private char letter; // The letter this button represents
-
-    /**
-     * Constructor for PlayerButton.
-     * 
-     * @param letter The letter this button represents.
-     */
-    public PlayerButton(char letter) {
-        super(String.valueOf(letter)); // Set the button's text to the letter
-        this.letter = letter;
-    }
-
-    /**
-     * Gets the letter represented by this button.
-     * 
-     * @return The letter represented by the button.
-     */
-    public char getLetter() {
-        return letter;
-    }
-
-    /**
-     * Adds an ActionListener to the button.
-     * 
-     * @param listener The ActionListener to associate with the button.
-     */
-    public void addWordListener(ActionListener listener) {
-        this.addActionListener(listener);
-    }
->>>>>>> branch 'main' of https://github.com/Javier2000-creator/CISC-191Project.git
 }
